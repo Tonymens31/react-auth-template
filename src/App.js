2e2c5a12e0ff5a930a16c4effe91ced83abe17a9
Anthony Mensah <60733737+Tonymens31@users.noreply.github.com>
2022-03-14 13:30:02 +0000

    push updates

--- src/App.js
+++ src/App.js
@@ -2,15 +2,17 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Login from "./components/views/Login";
 import Register from "./components/views/Register";
 import Forgot from "./components/views/Forgot";
+import Land from "./components/views/Land";
 
 const Auth = () => {
   return (
     <Router>
       <Switch>
+        <Route exact path="/" component={Land} />
         <Route path="/login" component={Login} />
         <Route path="/register" component={Register} />
         <Route path="/forgot-password" component={Forgot} />
-        <Route path="/" component={Login} />
+        <Route path="/land" component={Land} />
       </Switch>
     </Router>
   );
---
+++ src/components/views/Land.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import background from "../views/images/background.jpg";
+
+const Land = () => {
+  return (
+    <div className="row g-0 auth-wrapper">
+      <div
+        className="col-12 col-md-12 col-lg-12 h-100 "
+        style={{
+          backgroundImage: "url(" + background + ")",
+          backgroundPosition: "center",
+          backgroundSize: "cover",
+          backgroundRepeat: "no-repeat",
+        }}
+      >
+        <div style={{ marginTop: "23%" }}>
+          <div className="auth-option text-white text-center pt-2">
+            Click here to{" "}
+            <Link className="text-link" to="/login">
+              Login
+            </Link>
+          </div>
+          <div className="auth-option text-white text-center pt-2">
+            Click here to{" "}
+            <Link className="text-link" to="/register">
+              Client Registration
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Land;
